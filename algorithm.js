08098249125818d52ccfc50ed19c6678abed163b
scarletMaster <nadinebenarbia@gmail.com>
2022-11-09 13:07:55 +0100

    test technique

---
+++ algorithm.js
@@ -0,0 +1,11 @@
+const findIndex = (arr) => {
+  let max = 0;
+  let indexMax = 0;
+  let before = 0;
+  let after = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (indexOfarr[i] === 0) {
+    }
+  }
+};
+console.log(findIndex([1, 1, 0, 1, 1, 0, 1, 1]));
