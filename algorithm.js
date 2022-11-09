19bb9dbceed7c06cf59d9046c9dce2b7f58f39ed
scarletMaster <nadinebenarbia@gmail.com>
2022-11-09 13:14:15 +0100

    algo

--- algorithm.js
+++ algorithm.js
@@ -1,11 +1,20 @@
-const findIndex = (arr) => {
-  let max = 0;
-  let indexMax = 0;
-  let before = 0;
-  let after = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (indexOfarr[i] === 0) {
+var max = 1;
+var index = 0;
+var previndex = 0;
+var before = 0;
+var after = 0;
+var suit = [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1];
+for (let i = 0; i < suit.length; i++) {
+  if (suit[i] == 0) {
+    if (max < before + after) {
+      index = previndex;
+      max = before + after;
     }
+    before = after;
+    after = 0;
+    previndex = i;
+  } else {
+    after++;
   }
-};
-console.log(findIndex([1, 1, 0, 1, 1, 0, 1, 1]));
+}
+console.log(index);
